b974147d6d1054df760fb7b437a35d8088687bc7
Hoang Trung Tin <106093804+tinht04@users.noreply.github.com>
2025-10-26 20:21:44 +0700

    Remove commented connection string in NovaCMSDBContext
    Removed commented-out connection string configuration.

--- backend/NovaCMS.Infrastructure/Data/NovaCMSDBContext.cs
+++ backend/NovaCMS.Infrastructure/Data/NovaCMSDBContext.cs
@@ -39,9 +39,6 @@ public partial class NovaCMSDBContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseMySql("server=mysql-database-tinht-fptu.b.aivencloud.com;port=20277;database=NovaCMS;user=avnadmin;password=AVNS_9YwLqVnHoOLgvuOYaGJ", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.35-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
